ce3b7a91b2b251b289464f5ff387cf12961e729e
xule <463911347@qq.com>
2020-03-11 15:25:58 +0800

    3.11

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,1 +1,12 @@
-"use strict";
+$.get('http://jx.xuzhixiang.top/ap/api/bannerlist.php',{
+                'uid':'32977'
+            }).then(data=>{
+                let str = ''
+                for(let k in data.data){
+                    var a = data.data[k]
+                    console.log(a.banner_img_url)
+                        str +=` <div class="swiper-slide" style="background-image:url(./images/nature-1.jpg)"><img src="${a.banner_img_url}" alt=""></div>
+                        `
+                }
+                $('.sw1').html(str)
+            })
